3f52fb94e29fe0b0ce520758d78a696126c250b1
nmparfait <mbamu.developer@gmail.com>
2019-06-08 23:54:10 -0400

    script of changing the background

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello, tudo bem? ")
+// #E4DFDA, #D4B483, #C1666B, #4E878C, #00241B, #B5CA8D, #FCD581
+
+let colors = ['#E4DFDA', '#D4B483', '#C1666B', '#4E878C', '#00241B', '#B5CA8D', '#FCD581'];
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function(){
+    let random = Math.floor(Math.random()* colors.length);
+    let bckG = document.body;
+    bckG.style.backgroundColor =colors[random];
+})
+
